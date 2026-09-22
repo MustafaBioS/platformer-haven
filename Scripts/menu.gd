eaf4ac3f2e019a4647dba9240198ad787e54e7e9
MustafaBioS <mustaafahany@gmail.com>
2026-09-22 05:42:52 +0300

    feat: main menu

---
+++ Scripts/menu.gd
@@ -0,0 +1,27 @@
+extends Control
+@onready var settings_menu: ColorRect = $SettingsMenu
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_settings_pressed() -> void:
+	settings_menu.visible = true
+
+func _on_close_pressed() -> void:
+	settings_menu.visible = false
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
